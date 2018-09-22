906eea6730c57ab1499177c4fc374d7bb75bc7ca
shullmb <shullmb@gmail.com>
2018-09-22 10:35:55 -0700

    debug zipcode ommisions

--- seeders/clean_data.py
+++ seeders/clean_data.py
@@ -4,14 +4,17 @@ import numpy as np
 
 file_ext = '*.txt'
 dataframes = []
-test = pd.DataFrame()
+output = pd.DataFrame()
 
 for root, dirs, files in os.walk('./data'):
     for filename in files:
         if fnmatch.fnmatch(filename, file_ext):
-            dataframes.append(pd.read_fwf('./data/' + filename, dtype=str, header=None, sep='\s+'))
+            dataframes.append(pd.read_fwf('./data/' + filename, names=['Station', filename], dtype=str, header=None, sep='\s+'))
 
 for i in range(0,len(dataframes) - 1):
-    test = pd.merge(dataframes[i],dataframes[i+1], on=0, how='outer')
+    output = pd.merge(dataframes[i],dataframes[i+1], on='Station', how='outer')
 
-test.to_csv('./data/output.csv')
+zipcodes = pd.read_fwf('./data/zipcodes-normals-stations.txt', names=['Station', 'Zip','Location'],dtype=str, sep='\s+', header=None)
+
+output.merge(zipcodes, on='Station')
+output.to_csv('./data/output.csv', index=False)
