d348a147239381d7d31bb3c50ad795c02ba48df5
shullmb <shullmb@gmail.com>
2018-09-22 10:51:12 -0700

    data merging correctly

--- seeders/clean_data.py
+++ seeders/clean_data.py
@@ -2,19 +2,20 @@ import os, fnmatch
 import pandas as pd
 import numpy as np
 
-file_ext = '*.txt'
+file_ext = 'ann-*.txt'
 dataframes = []
-output = pd.DataFrame()
+output = None;
+zipcodes = pd.read_fwf('./data/zipcodes-normals-stations.txt', names=['Station', 'Zip','Location'],dtype=str, sep='\s+', header=None)
 
 for root, dirs, files in os.walk('./data'):
     for filename in files:
         if fnmatch.fnmatch(filename, file_ext):
             dataframes.append(pd.read_fwf('./data/' + filename, names=['Station', filename], dtype=str, header=None, sep='\s+'))
 
-for i in range(0,len(dataframes) - 1):
-    output = pd.merge(dataframes[i],dataframes[i+1], on='Station', how='outer')
+for i in range(0,len(dataframes)):
+    output = pd.merge(zipcodes, dataframes[i], on='Station', how='inner')
+    zipcodes = output
 
-zipcodes = pd.read_fwf('./data/zipcodes-normals-stations.txt', names=['Station', 'Zip','Location'],dtype=str, sep='\s+', header=None)
 
-output.merge(zipcodes, on='Station')
-output.to_csv('./data/output.csv', index=False)
+# output.merge(zipcodes, on='Station')
+zipcodes.to_csv('./data/output.csv', index=False)
