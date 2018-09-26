3ca2bc152eb98006e738e9fe0f6722c92adbafd7
shullmb <shullmb@gmail.com>
2018-09-25 19:45:59 -0700

    seeder seeding

--- seeders/20180925234648-frostdates.js
+++ seeders/20180925234648-frostdates.js
@@ -1,26 +1,56 @@
 'use strict';
+const db = require('../models');
+const raw = require('./data/output.json');
+const bulkArr = [];
+const date = new Date();
+
+raw.forEach(obj => {
+  bulkArr.push({
+    station: obj['Station'],
+    zipcode: obj['Zip'],
+    location: obj['Location'],
+    fst24p50: obj['prbfst-t24Fp50'],
+    fst24p60: obj['prbfst-t24Fp60'],
+    fst24p70: obj['prbfst-t24Fp70'],
+    fst24p80: obj['prbfst-t24Fp80'],
+    fst24p90: obj['prbfst-t24Fp90'],
+    fst28p50: obj['prbfst-t28Fp50'],
+    fst28p60: obj['prbfst-t28Fp60'],
+    fst28p70: obj['prbfst-t28Fp70'],
+    fst28p80: obj['prbfst-t28Fp80'],
+    fst28p90: obj['prbfst-t28Fp90'],
+    fst32p50: obj['prbfst-t32Fp50'],
+    fst32p60: obj['prbfst-t32Fp60'],
+    fst32p70: obj['prbfst-t32Fp70'],
+    fst32p80: obj['prbfst-t32Fp80'],
+    fst32p90: obj['prbfst-t32Fp90'],
+    lst24p50: obj['prblst-t24Fp50'],
+    lst24p60: obj['prblst-t24Fp60'],
+    lst24p70: obj['prblst-t24Fp70'],
+    lst24p80: obj['prblst-t24Fp80'],
+    lst24p90: obj['prblst-t24Fp90'],
+    lst28p50: obj['prblst-t28Fp50'],
+    lst28p60: obj['prblst-t28Fp60'],
+    lst28p70: obj['prblst-t28Fp70'],
+    lst28p80: obj['prblst-t28Fp80'],
+    lst28p90: obj['prblst-t28Fp90'],
+    lst32p50: obj['prblst-t32Fp50'],
+    lst32p60: obj['prblst-t32Fp60'],
+    lst32p70: obj['prblst-t32Fp70'],
+    lst32p80: obj['prblst-t32Fp80'],
+    lst32p90: obj['prblst-t32Fp90'],
+    createdAt: date,
+    updatedAt: date
+  })
+})
+
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    return queryInterface.bulkInsert('frostdates', bulkArr, {})
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('Person', null, {});
-    */
+    return queryInterface.bulkDelete('frostdates', null, {})
   }
 };
--- seeders/csvtojson.js
+++ seeders/csvtojson.js
@@ -1,13 +1,11 @@
 const fs = require('fs');
 const csv = require('csvtojson');
-// const db = require('../models');
 const INPUT = './data/output.csv';
 
 const parameters = {
     delimiter: ',',
     trim: false
 }
-
 csv(parameters).fromFile(INPUT).then( arr => {
     fs.writeFile('./data/output.json', JSON.stringify(arr))
 })
