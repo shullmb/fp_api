84ab27220d0c59936a7ebdd8b8e022a57be7403d
Michael Shull <shullmb@gmail.com>
2018-10-09 18:48:40 -0700

    restructuring return object

--- controllers/frostdates.js
+++ controllers/frostdates.js
@@ -19,42 +19,59 @@ router.get('/:zip', (req, res) => {
 		// refactor to clean up return objects in seq hook
 		let returnArr = arr.map( report => {
 			let retObj = {
-					station: report.station,
-					zipcode: report.zipcode,
-					location: report.location,
-					fst24p50: report.fst24p50,
-					fst24p60: report.fst24p60,
-					fst24p70: report.fst24p70,
-					fst24p80: report.fst24p80,
-					fst24p90: report.fst24p90,
-					fst28p50: report.fst28p50,
-					fst28p60: report.fst28p60,
-					fst28p70: report.fst28p70,
-					fst28p80: report.fst28p80,
-					fst28p90: report.fst28p90,
-					fst32p50: report.fst32p50,
-					fst32p60: report.fst32p60,
-					fst32p70: report.fst32p70,
-					fst32p80: report.fst32p80,
-					fst32p90: report.fst32p90,
-					lst24p50: report.lst24p50,
-					lst24p60: report.lst24p60,
-					lst24p70: report.lst24p70,
-					lst24p80: report.lst24p80,
-					lst24p90: report.lst24p90,
-					lst28p50: report.lst28p50,
-					lst28p60: report.lst28p60,
-					lst28p70: report.lst28p70,
-					lst28p80: report.lst28p80,
-					lst28p90: report.lst28p90,
-					lst32p50: report.lst32p50,
-					lst32p60: report.lst32p60,
-					lst32p70: report.lst32p70,
-					lst32p80: report.lst32p80,
-					lst32p90: report.lst32p90
+				station: report.station,
+				zipcode: report.zipcode,
+				location: report.location,
+				first_date: {
+					hard_freeze_24: {
+						p50: report.fst24p50,
+						p60: report.fst24p60,
+						p70: report.fst24p70,
+						p80: report.fst24p80,
+						p90: report.fst24p90
+					},
+					freeze_28: {
+						p50: report.fst28p50,
+						p60: report.fst28p60,
+						p70: report.fst28p70,
+						p80: report.fst28p80,
+						p90: report.fst28p90
+					},
+					frost_32: {
+						p50: report.fst32p50,
+						p60: report.fst32p60,
+						p70: report.fst32p70,
+						p80: report.fst32p80,
+						p90: report.fst32p90
+					}
+				},
+				last_date: {
+					hard_freeze_24: {
+						p50: report.lst24p50,
+						p60: report.lst24p60,
+						p70: report.lst24p70,
+						p80: report.lst24p80,
+						p90: report.lst24p90
+					},
+					freeze_28: {
+						p50: report.lst28p50,
+						p60: report.lst28p60,
+						p70: report.lst28p70,
+						p80: report.lst28p80,
+						p90: report.lst28p90
+					},
+					frost_32: {
+						p50: report.lst32p50,
+						p60: report.lst32p60,
+						p70: report.lst32p70,
+						p80: report.lst32p80,
+						p90: report.lst32p90
+					}
+				}
 			}
 			return retObj;
 		})
+
 		res.json(returnArr);
 	})
 	.catch(err => res.send(err))
